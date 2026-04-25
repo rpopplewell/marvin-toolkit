c2fe0147f40f34f73016bb35a472dc95e31097bb
Christopher Dickerman <chrisdickerman.main@gmail.com>
2026-04-24 19:40:10 -0500

    Update shell script for regenerating more probes for smaller CIs

--- step2-alt.sh
+++ step2-alt.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 
+rm -rf rsa1024_repeat rsa2048_repeat rsa4096_repeat
+
 PYTHONPATH=tlsfuzzer ./marvin-venv/bin/python ./step2.py \
 -c rsa1024/cert.pem -o rsa1024_repeat \
---repeat 100000 --verbose \
+--repeat 1000000 --verbose \
 no_structure no_padding=48 signature_padding=8 \
 valid_repeated_byte_payload="118 0xff" \
 valid_repeated_byte_payload="118 0x01" \
 valid=48 header_only \
 no_header_with_payload=48 zero_byte_in_padding="48 4" \
-valid=0 valid=118
+valid=0 valid=118 &
 
 PYTHONPATH=tlsfuzzer ./marvin-venv/bin/python ./step2.py \
 -c rsa2048/cert.pem -o rsa2048_repeat \
---repeat 100000 --verbose \
+--repeat 1000000 --verbose \
 no_structure no_padding=48 signature_padding=8 \
 valid_repeated_byte_payload="246 0xff" \
 valid_repeated_byte_payload="246 0x01" \
 valid=48 header_only \
 no_header_with_payload=48 zero_byte_in_padding="48 4" \
-valid=0 valid=192 valid=246
+valid=0 valid=192 valid=246 &
 
 PYTHONPATH=tlsfuzzer ./marvin-venv/bin/python ./step2.py \
 -c rsa4096/cert.pem -o rsa4096_repeat \
---repeat 100000 --verbose \
+--repeat 1000000 --verbose \
 no_structure no_padding=48 signature_padding=8 \
 valid_repeated_byte_payload="502 0xff" \
 valid_repeated_byte_payload="502 0x01" \
 valid=48 header_only \
 no_header_with_payload=48 zero_byte_in_padding="48 4" \
-valid=0 valid=192 valid=502
+valid=0 valid=192 valid=502 &
+
+wait
